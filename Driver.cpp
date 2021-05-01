487b2698e9d7d892c0834d564d8da90fa8ff6b5d
omerc <omerceylan38@gmail.com>
2021-05-02 00:12:46 +0300

    multiple SPI and Semaphore will be add

--- Driver.cpp
+++ Driver.cpp
@@ -1,37 +1,39 @@
 #include <iostream>
 #include <stdint.h>
 
+typedef enum
+{
+    MOSI_PIN_0 = 11,
+    MOSI_PIN_1 = 12,
+    MOSI_PIN_2 = 13
+} SPI_Mosi;
+
+typedef enum
+{
+    MISO_PIN_0 = 21,
+    MISO_PIN_1 = 22,
+    MISO_PIN_2 = 23
+} SPI_Miso;
+
+typedef enum
+{
+    CS_PIN_0 = 31,
+    CS_PIN_1 = 32,
+    CS_PIN_2 = 33,
+    CS_Default
+} SPI_Cs;
+
+typedef enum
+{
+    CLK_PIN_0 = 41,
+    CLK_PIN_1 = 42,
+    CLK_PIN_2 = 43
+} SPI_Clk;
+
 class MCU_PINS
 {
 
 public:
-    typedef enum
-    {
-        MOSI_PIN_0 = 11,
-        MOSI_PIN_1 = 12,
-        MOSI_PIN_2 = 13
-    } SPI_Mosi;
-
-    typedef enum
-    {
-        MISO_PIN_0 = 21,
-        MISO_PIN_1 = 22,
-        MISO_PIN_2 = 23
-    } SPI_Miso;
-
-    typedef enum
-    {
-        CS_PIN_0 = 31,
-        CS_PIN_1 = 32,
-        CS_PIN_2 = 33
-    } SPI_Cs;
-
-    typedef enum
-    {
-        CLK_PIN_0 = 41,
-        CLK_PIN_1 = 42,
-        CLK_PIN_2 = 43
-    } SPI_Clk;
 };
 
 class Com : public MCU_PINS
@@ -59,26 +61,33 @@ public:
         case SPI_0:
             this->mosi = MOSI_PIN_0;
             this->miso = MISO_PIN_0;
-            this->cs = CS_PIN_0;
             this->clk = CLK_PIN_0;
+            this->cs = CS_PIN_0;
             break;
         case SPI_1:
             this->mosi = MOSI_PIN_1;
             this->miso = MISO_PIN_1;
-            this->cs = CS_PIN_1;
             this->clk = CLK_PIN_1;
+            this->cs = CS_PIN_1;
             break;
         case SPI_2:
             this->mosi = MOSI_PIN_2;
             this->miso = MISO_PIN_2;
-            this->cs = CS_PIN_2;
             this->clk = CLK_PIN_2;
+            this->cs = CS_PIN_2;
             break;
         default:
             //error handling
             break;
         }
-        this->mosi = mosi;
+    }
+
+    SPI_Com(SPI_Cs Cs, SPI_Miso Miso, SPI_Mosi Mosi, SPI_Clk Clk)
+    {
+        this->mosi = Mosi;
+        this->miso = Miso;
+        this->clk = Clk;
+        this->cs = Cs;
     }
 
     void sent()
@@ -91,7 +100,7 @@ public:
 
     void SystemMonitor()
     {
-        std::cout << this->mosi << std::endl;
+        std::cout << this->cs << std::endl;
     }
 
     ~SPI_Com()
@@ -104,12 +113,13 @@ private:
     SPI_Cs cs;
     SPI_Clk clk;
 };
-
+#define semaphore int
 class GPS
 {
 
 public:
     SPI_Com *SPI;
+    semaphore semaphoreGps;
     GPS(SPI_Com &SPI)
     {
         this->SPI = &SPI;
@@ -118,8 +128,10 @@ public:
 
     void GetGPS_Speed()
     {
-        this->SPI->sent();    // sent command
-        this->SPI->receive(); // receive message
+        if(semaphoreGps == 1){ //xsemaphoreTake
+
+        }
+        this->SPI->SystemMonitor();
     }
 };
 
@@ -127,6 +139,12 @@ int main()
 {
 
     SPI_Com SPI0(SPI_0);
+    SPI_Com SPI0_1(CS_PIN_0, MISO_PIN_1, MOSI_PIN_2, CLK_PIN_0);
+
     GPS GPS1(SPI0);
+    GPS1.GetGPS_Speed();
+    GPS GPS2(SPI0_1);
+    GPS2.GetGPS_Speed();
+
     return 0;
 }
